f9f639dd1c6e5fbad7d2d5a05e675e0ffc8f84b2
Gediminas Ivanauskas <gediminas.iv@gmail.com>
2022-04-04 15:57:23 +0300

    Refactor old form

--- tasks/form.php
+++ tasks/form.php
@@ -1,47 +1,8 @@
 <?php
 
-class MovieForm extends FileManager
+class Form
 {
     public $formInputs;
-
-    function __construct($formInputs)
-    {
-        parent::__construct('data/filmai.json');
-        $this->formInputs = $formInputs;
-    }
-
-    function processFormRequest($postData)
-    {
-        if (isset($postData['submit'])) {
-            $filmJson = $this->readJsonToArray();
-
-            $filename = 'uploads/' . $_FILES['fileToUpload']['name'];
-
-            move_uploaded_file($_FILES['fileToUpload']['tmp_name'], $filename);
-
-            $newMovie = [
-                'movieType' => $_POST['movieType'],
-                'title' => $_POST['title'],
-                'synopsis' => $_POST['synopsis'],
-                'year' => $_POST['year'],
-                'imageUrl' => $filename
-            ];
-
-            if ($_POST['movieType'] === 'cinema') {
-                $newMovie['premiereDate'] = $_POST['date'];
-                $newMovie['ticketPrice'] = $_POST['price'];
-            } else {
-                $newMovie['rentalDuration'] = $_POST['date'];
-                $newMovie['rentalPrice'] = $_POST['price'];
-            }
-
-            $filmJson[] = $newMovie;
-
-            $this->writeArrayToJson($filmJson);
-
-            header('Location: /?page=movies');
-        }
-    }
 
     function generateInputHtml($name, $placeholder, $type = 'text')
     {
